e404680c935fd2370e4242052ad1cddb987fd689
EISEI TEZUKA <eisei0504@outlook.jp>
2024-12-26 22:12:33 +0900

    [update]スクレイピング完成

--- scraping.py
+++ scraping.py
@@ -12,3 +12,18 @@ today = soup.find("section", attrs ={"id":"uamods-topics"})
 
 # ヒットしたTagクラスのインスタンスのリストをリターンする
 entries = today.find_all("li")
+# CSV出力用リスト
+today_list = []
+index = 1
+
+for entry in entries:
+  title = entry.get_text()
+  entry_url = entry.find("a").get("href")
+  today_list.append([index, title, entry_url])
+  index += 1
+
+dt_now = datetime.datetime.now()
+
+with open(str(dt_now.year)+"_"+str(dt_now.month)+"_"+str(dt_now.day)+"_"+str(dt_now.hour)+str(dt_now.minute)+"_NewsTopics.csv", "w") as file:
+  writer = csv.writer(file,lineterminator='\n')
+  writer.writerows(today_list)
